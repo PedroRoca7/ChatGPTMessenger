2e476db1ce06778853987b5a8c449ed386a0ab93
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-05-10 19:05:47 -0300

    Commit
    Resolvido a constrain da view para subir junto com o teclado.

--- ChatGPTMessenger/ViewController.swift
+++ ChatGPTMessenger/ViewController.swift
@@ -13,16 +13,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var inputMessageView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     override func viewWillAppear(_ animated: Bool) {
-        messageBarView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor).isActive = true
         tableView.transform = CGAffineTransform(scaleX: 1, y: -1)
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Registrar observadores para notificações de teclado
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // Método chamado quando o teclado está prestes a aparecer
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            // Obtenha a altura do teclado
+            let keyboardHeight = keyboardSize.height
+            
+            // Atualize a constante de constraint vertical inferior com a altura do teclado
+            bottomConstraint.constant = -keyboardHeight
+            
+            // Animação da atualização da constraint
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    // Método chamado quando o teclado está prestes a desaparecer
+    @objc func keyboardWillHide(notification: NSNotification) {
+        // Restaure a constante de constraint vertical inferior para o valor original (0)
+        bottomConstraint.constant = 0
+        
+        // Animação da atualização da constraint
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // Certifique-se de remover os observadores de notificação quando a view controller for destruída
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     @IBAction func sendMessage(_ sender: Any) {
