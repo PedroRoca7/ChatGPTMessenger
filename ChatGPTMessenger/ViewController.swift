4f5dd7b28e0a43af54ffc470bc7e83fc5bc164cb
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2023-05-09 23:07:44 -0300

    Commit constrains
    Colocado constrains no textfield para subir junto com teclado

--- ChatGPTMessenger/ViewController.swift
+++ ChatGPTMessenger/ViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageBarView: UIView!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        messageBarView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor).isActive = true
+        
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func sendMessage(_ sender: Any) {
+    }
+    
 
 
 }
